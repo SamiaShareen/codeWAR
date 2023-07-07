38a1faa5c7912c001d21328d367674d25643f6b5
Samia Shareen <samiashareen369@gmail.com>
2023-07-07 11:55:39 +0600

    :|

---
+++ Sheet_A/CF405-D2-A.cpp
@@ -0,0 +1,49 @@
+// Bismillahir Rahmanir Raheem
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    int n,a[1000],i,j,t;
+    cin >> n;
+    for(i=0;i<n;i++){
+        cin >> a[i];
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        cout << a[i] << ' ';
+    }
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
