78716244eeb32c4a73818a7f437c98dacd5b2125
Samia Shareen <samiashareen369@gmail.com>
2023-07-10 09:54:35 +0600

    AC

---
+++ Sheet_A/CF427-D2-A.cpp
@@ -0,0 +1,45 @@
+// Bismillahir Rahmanir Raheem
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    int n,cnt=0,i,s=0,f=0;
+    cin >> n;
+    vi a(n);
+    forcin(a);
+    for(i=0;i<n;i++){
+        if(a[i]==-1 && s==0){
+            cnt++;
+        }
+        else{
+            s+=a[i];
+        }
+    }
+    cout << cnt << el;
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
